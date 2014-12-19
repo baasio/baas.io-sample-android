6f7baff91857324d6b2efd7ebbdc9b2ccaac5bdf
홍 성훈 <brad.hong@kt.com>
2014-12-19 16:27:15 +0900

    no message

--- BaasioSampleProject/src/com/kth/baasio/sample/BaasioConfig.java
+++ BaasioSampleProject/src/com/kth/baasio/sample/BaasioConfig.java
@@ -5,10 +5,10 @@ public class BaasioConfig {
     public static final String BAASIO_URL = "https://api.baas.io";
 
     // baas.io member ID
-    public static final String BAASIO_ID = "6425e8e4-c2a3-11e2-9aa5-06fd000000c2";
+    public static final String BAASIO_ID = "PUT_YOUR_MEMBER_ID";
 
     // baas.io application ID
-    public static final String APPLICATION_ID = "64424b4e-c2a3-11e2-9aa5-06fd000000c2";
+    public static final String APPLICATION_ID = "PUT_YOUR_APPLICATION_ID";
 
     // GCM(google cloud messaging service) sender ID
     public static final String GCM_SENDER_ID = null;
